b3c1d747a3f98f2b4932f2ed1a4710da6e9a94b7
Carlos Toledo <105874879+to1edo@users.noreply.github.com>
2022-06-15 19:19:11 -0400

    first commit

---
+++ includes/config/database.php
@@ -0,0 +1,13 @@
+<?php 
+
+function conectarDB() : mysqli {
+    $db = mysqli_connect('localhost', 'root', 'root', 'bienesraices');
+
+    if(!$db) {
+        echo "Error no se pudo conectar";
+        exit;
+    } 
+
+    return $db;
+    
+}
